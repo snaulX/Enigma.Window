4004f1dcb92edf89a1c3c9f4e25caf7b24a021a2
snaulX <alexandergunger666@gmail.com>
2021-10-17 13:12:33 +0600

    Rework module system

--- src/base.h
+++ src/base.h
@@ -4,6 +4,16 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#if defined(NULL)
+#undef NULL
+#endif
+
+#if defined(__cplusplus)
+#define NULL 0
+#else
+#define NULL ((void*)0)
+#endif
+
 #define STRUCT(name) typedef struct name name; \
     struct name
 
@@ -12,19 +22,32 @@
     body                                                           \
     };                                                             \
     union { base* Parent; name Child; } base##To##name##Convert;   \
-    name Get##name##From##base(base* _base)                        \
+    inline name Get##name##From##base(base* _base)                        \
 {                                                                  \
     base##To##name##Convert.Parent = _base;                        \
     return base##To##name##Convert.Child;                          \
 }                                                                  \
-    base* Get##base##From##name(name _child)                        \
+    inline base* Get##base##From##name(name _child)                       \
 {                                                                  \
     base##To##name##Convert.Child = _child;                        \
-    return base##To##name##Convert.Parent;                          \
+    return base##To##name##Convert.Parent;                         \
 }
 
-
+#ifdef WIN32
+    #define PUBLIC __declspec(dllexport)
+    #define PRIVATE
+#else
+    #define PUBLIC __attribute__((visibility("default")))
+    #define PRIVATE __attribute__((visibility("internal")))
+#endif
 
 #define METHOD(rtype, name) rtype(*name)
+#define DECLMETHOD(rtype, name, params) PUBLIC METHOD(rtype, name) params; \
+    PUBLIC void Set_##name(METHOD(rtype, _##name)params);
+
+#define SETMETHOD(rtype, name, params) void Set_##name(METHOD(rtype, _##name)params) { \
+name = _##name; }
+#define GENPUBMETHOD(rtype, name, params) PUBLIC METHOD(rtype, name) params; \
+PUBLIC inline SETMETHOD(rtype, name, params)
 
 #endif //ENIGMA_WINDOW_BASE_H
--- src/glfw/GlfwWindow.h
+++ src/glfw/GlfwWindow.h
@@ -3,7 +3,10 @@
 #include "WindowModule.h"
 
 STRUCT_EXTENDS(GlfwWindow, Window,
-    METHOD(void, Init)(GlfwWindow*);
+               const char* test;
 )
+
+PRIVATE const char* Glfw__GetTitle(Window*);
+PRIVATE void Glfw__SetTitle(Window*, const char*);
 
 #endif //ENIGMA_WINDOW_GLFWWINDOW_H
